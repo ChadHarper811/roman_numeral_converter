3cb4b53924a9143d55145222df0ff694f5e18318
Chad Harper <chadharper811@gmail.com>
2024-08-24 13:27:07 -0500

    removing extra code, change const to new ids and new project requirements

--- script.js
+++ script.js
@@ -1,41 +1,19 @@
-const userInputText = document.getElementById("text-input");
-const checkBtn = document.getElementById("check-btn");
-const resultDiv = document.getElementById("result");
-const definitionTarget = document.getElementById("definition-target");
-const definitionDiv = document.getElementById("definition-div");
-const resultText = document.getElementById("result-text");
-const closeBtn = document.getElementById("close-btn");
+const userInput = document.getElementById("number");
+const convertBtn = document.getElementById("convert-btn");
+const outputDiv = document.getElementById("output");
+const outputText = document.getElementById("output-text");
 
-const palindromeCheck = str => {
+
+const convertToRomanNumeral = str => {
     const userInputStr = str;
 
     if (str === "") {
         alert("Please input a value");
         return;
     }
-
-    const compareStr = str.replace(/[^a-z0-9]/gi, "").toLowerCase();
-    console.log(compareStr)
-    let results = "";
-    if (compareStr === [...compareStr].reverse().join("")) {
-        results = `<bold>${userInputStr}</bold> is a palindrome.`
-    } else {
-        results = `<bold>${userInputStr}</bold> is not a palindrome.`
-    }
-
-    resultText.innerHTML = results;
-    resultDiv.classList.remove("hidden");
 }
 
-definitionTarget.addEventListener("click", () => {
-    definitionDiv.classList.contains("hidden") ? definitionDiv.classList.remove("hidden") : definitionDiv.classList.add("hidden");
-});
-
-closeBtn.addEventListener("click", () => {
-    definitionDiv.classList.contains("hidden") ? definitionDiv.classList.remove("hidden") : definitionDiv.classList.add("hidden");
-})
-
-checkBtn.addEventListener("click", () => {
-    palindromeCheck(userInputText.value);
+convertBtn.addEventListener("click", () => {
+    convertToRomanNumeral(userInput.value);
     userInputText.value = "";
 })
