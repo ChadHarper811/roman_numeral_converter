2800959a55264b34ccb67fd5cfd3b87415766b2c
Chad Harper <chadharper811@gmail.com>
2024-08-24 13:54:31 -0500

    adding convertion loop into function

--- script.js
+++ script.js
@@ -11,7 +11,27 @@ const convertToRomanNumeral = number => {
         outputText.innerText = "Please enter a valid number";
     } else if (number < 0) {
         outputText.innerText = "Please enter a number greater than or equal to 1";
+    } else if (number > 3999) {
+        outputText.innerText = "Please enter a number less than or equal to 3999"
+    } else {
+        let romanNumeralOuput = "";
+        while (number >= 0) {
+            if (number > 1000) {
+                number - 1000;
+                romanNumeralOuput += "M";
+            } else if (number >= 900) {
+                number - 900;
+                romanNumeralOuput += "CM";
+            } else if (number >= 500) {
+                number - 500;
+                romanNumeralOuput += "D";
+            } else if (number >= 400) {
+                number - 400;
+                romanNumeralOuput += "CD";
+            }
+        }
     }
+
 
     outputDiv.classList.remove("hidden");
 }
